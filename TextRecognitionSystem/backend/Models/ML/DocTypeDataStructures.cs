692e2145d9ef22441546fa1b13142f0a56d85bd1
Vitaliy Datsyshyn <Vitaliy_Datsyshyn@epam.com>
2020-05-08 09:06:07 +0300

    Implemented doc type predicting

---
+++ TextRecognitionSystem/backend/Models/ML/DocTypeDataStructures.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+
+namespace backend.Models.ML
+{
+    public class DocTypeInput
+    {
+        [LoadColumn(0)]
+        public string DocType;
+
+        [LoadColumn(1)]
+        public string DocText;
+    }
+
+    public class DocsTypePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string DocType;
+
+        [ColumnName("Score")]
+        public float[] Score;
+    }
+}
---
+++ TextRecognitionSystem/backend/TextPostprocessing/DocTypePredictor.cs
@@ -0,0 +1,37 @@
+﻿using backend.Helpers;
+using backend.Models.ML;
+using Microsoft.ML;
+using System;
+
+namespace backend.TextPostprocessing
+{
+    public class DocTypePredictor
+    {
+        private string _docText;
+        private string _pathToModel;
+
+        public DocTypePredictor(string docText, string pathToModel)
+        {
+            _docText = docText;
+            _pathToModel = pathToModel;
+        }
+
+        public string Predict()
+        {
+            try
+            {
+                FileHelper.CheckFilePathExisting(_pathToModel);
+                MLContext mlContext = new MLContext(seed: 1);
+                ITransformer loadedModel = mlContext.Model.Load(_pathToModel, out var columns);
+                var predictor = mlContext.Model.CreatePredictionEngine<DocTypeInput, DocsTypePrediction>(loadedModel);
+                var predictionResult = predictor.Predict(new DocTypeInput() { DocText = _docText });
+                return predictionResult.DocType;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return String.Empty;
+            }
+        }
+    }
+}
